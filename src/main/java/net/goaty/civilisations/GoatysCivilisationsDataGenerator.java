7927add6b289214dfcd9cd2166455a52abfd135d
Goaty1208 <goaty1208@gmail.com>
2024-11-20 14:34:25 +0100

    initial commit

---
+++ src/main/java/net/goaty/civilisations/GoatysCivilisationsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.goaty.civilisations;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GoatysCivilisationsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
